f86a8678189b2fd33d8fa628cdea79225ac9808f
thangngo <ngosythang123456@gmail.com>
2024-11-06 00:53:32 +0700

    feat: list product

--- src/app/(shop)/index.tsx
+++ src/app/(shop)/index.tsx
@@ -1,9 +1,29 @@
-import { Text, View } from "react-native";
+import { FlatList, StyleSheet, Text, View } from "react-native";
+import { PRODUCTS } from "../../../assets/products";
+import { ProductListItem } from "../components/product-list-item";
 
 export default function Home() {
   return (
     <View>
-      <Text>Home</Text>
+      <FlatList
+        data={PRODUCTS}
+        renderItem={({ item }) => <ProductListItem product={item} />}
+        keyExtractor={(item) => item.imagesUrl.toString()}
+        numColumns={2}
+        ListHeaderComponent={<Text>Products</Text>}
+        contentContainerStyle={styles.flatListContent}
+        columnWrapperStyle={styles.flatListColumn}
+        style={{ paddingHorizontal: 10, paddingVertical: 5 }}
+      />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  flatListContent: {
+    paddingBottom: 20,
+  },
+  flatListColumn: {
+    justifyContent: "space-between",
+  },
+});
